42f4899ea9ff664e13d1894a8ad5c3af1d0b360a
HanzMarin <wilyhanz@gmail.com>
2026-08-05 12:12:24 -0500

    Fix image path case sensitivity in page1.js

--- JS/PAGES/page1.js
+++ JS/PAGES/page1.js
@@ -5,7 +5,7 @@ export const page1 = {
 
   render: function() {
     return `
-      <div class="story-page page1-container" style="background-image: url('img/fondo1.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+      <div class="story-page page1-container" style="background-image: url('IMG/fondo1.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">
         <!-- Personajes PNG más grandes y animados -->
         <img src="img/niños_arena.png" alt="Niños jugando en la arena" class="page1-kids-sand">
 
